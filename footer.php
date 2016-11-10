bc7717a8a75f114fe8f5885d4744cb8edcb3b094
pixelasticity <contact@alexanderbell.info>
2016-11-10 12:39:43 -0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,7 +6,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
  *
- * @package _s
+ * @package WP_REST_API_Theme
  */
 
 ?>
@@ -15,9 +15,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', '_s' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wp-rest-api-theme' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'wp-rest-api-theme' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'wp-rest-api-theme' ), 'wp-rest-api-theme', '<a href="https://alexanderbell.info" rel="designer">abelluscio</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
